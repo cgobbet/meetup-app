0f1037feb3a51f8c108501cd63ffe535beb88579
cgobbet <cassiano.gobbet@gmail.com>
2020-02-06 15:54:15 +0100

    Updates

---
+++ precache-manifest.2f2291cdd3f223f6ab8fe4ab392daf1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70be55b7dbd8f1670f95aaf7185845d8",
+    "url": "/meetup-app/index.html"
+  },
+  {
+    "revision": "18af9f3ca38e96d25ba4",
+    "url": "/meetup-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3ee8d8a1096c7ef0d46a",
+    "url": "/meetup-app/static/js/2.efea5617.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/meetup-app/static/js/2.efea5617.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18af9f3ca38e96d25ba4",
+    "url": "/meetup-app/static/js/main.a08a2353.chunk.js"
+  },
+  {
+    "revision": "8255df014c25e6950d38",
+    "url": "/meetup-app/static/js/runtime-main.7b185fef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meetup-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
