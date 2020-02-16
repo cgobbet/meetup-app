e905bcfc94aa1dbf3afb1ffedce92d7fd5a784bf
cgobbet <cassiano.gobbet@gmail.com>
2020-02-16 13:58:26 +0100

    4.2 submission

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -24,3 +24,25 @@ module.exports.getAccessToken = async (event) => {
       }),
     };
 };
+
+module.exports.refreshAccessToken = async (event) => {
+
+  const MEETUP_OAUTH_URL = 'https://secure.meetup.com/oauth2/access'
+    + '?client_id=l2jcfng9l83dsd3qe5vb48988n'
+    + '&client_secret=4010b2b0e6k9u8ojghfjkf8v8u'
+    + '&grant_type=refresh_token'
+    + '&refresh_token=' + event.pathParameters.refresh_token;
+
+  const info = await axios.post(MEETUP_OAUTH_URL);
+
+  return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        access_token: info.data.access_token,
+        refresh_token: info.data.refresh_token,
+      }),
+    };
+};
