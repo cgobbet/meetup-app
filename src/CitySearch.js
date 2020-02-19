735fd29d1f7351513b15d032bbc72d64409a9fed
cgobbet <cassiano.gobbet@gmail.com>
2020-02-19 19:53:23 +0100

    CitySearch.js after "change state when text input changes" test

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 
 class CitySearch extends Component {
+  state = { // define query state for CitySearch component
+    query: 'Munich',
+  }
+
+  handleInputChanged = (event) => {
+  const value = event.target.value;
+  this.setState({ query: value });
+  }
   render() {
     return (
       <div className='CitySearch'>
-        <input 
-          type='text' 
-          className='city' />
+        <input // adds city element
+          type='text'
+          className='city'
+          value={this.state.query} //<input> will derive its value from the value of query (which is located in the state of CitySearch).
+          onChange={this.handleInputChanged}
+        />
+        <ul className='suggestions'></ul>
       </div>
     );
   }
