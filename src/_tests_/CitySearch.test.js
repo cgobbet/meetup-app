38aff0f169a469572dca15800453a7f824a83f01
cgobbet <cassiano.gobbet@gmail.com>
2020-02-19 19:53:43 +0100

    Update after "change state when text input changes" test

--- src/_tests_/CitySearch.test.js
+++ src/_tests_/CitySearch.test.js
@@ -3,8 +3,24 @@ import React from "react";
 import { shallow } from "enzyme";
 
 describe("<CitySearch /> component", () => {
+  let CitySearchWrapper;
+  beforeAll(() => {
+    CitySearchWrapper = shallow(<CitySearch />);
+  });
   test("render text input", () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find(".city")).toHaveLength(1);
   });
+  test("render list of suggestions", () => {
+    expect(CitySearchWrapper.find(".suggestions")).toHaveLength(1);
+  });
+  test("render text input correctly", () => {
+    const query = CitySearchWrapper.state("query");
+    expect(CitySearchWrapper.find(".city").prop("value")).toBe(query);
+  });
+    test("change state when text input changes", () => {
+    const CitySearchWrapper = shallow(<CitySearch />);
+    const eventObject = { target: { value: "Berlin" } }; 
+    CitySearchWrapper.find(".city").simulate("change", eventObject);
+    expect(CitySearchWrapper.state("query")).toBe("Berlin");
+    });
 });
