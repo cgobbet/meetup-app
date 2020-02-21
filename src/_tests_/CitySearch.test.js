b8d5baea4bdf544fefb321506cc07a387ebfa39b
cgobbet <cassiano.gobbet@gmail.com>
2020-02-21 14:37:48 +0100

    City location, search and suggestions tests done

--- src/_tests_/CitySearch.test.js
+++ src/_tests_/CitySearch.test.js
@@ -8,19 +8,60 @@ describe("<CitySearch /> component", () => {
     CitySearchWrapper = shallow(<CitySearch />);
   });
   test("render text input", () => {
-    expect(CitySearchWrapper.find(".city")).toHaveLength(1);
+      expect(CitySearchWrapper.find(".city")).toHaveLength(1);
   });
   test("render list of suggestions", () => {
-    expect(CitySearchWrapper.find(".suggestions")).toHaveLength(1);
+      expect(CitySearchWrapper.find(".suggestions")).toHaveLength(1);
   });
   test("render text input correctly", () => {
     const query = CitySearchWrapper.state("query");
-    expect(CitySearchWrapper.find(".city").prop("value")).toBe(query);
+      expect(CitySearchWrapper.find(".city").prop("value")).toBe(query);
   });
-    test("change state when text input changes", () => {
-    const CitySearchWrapper = shallow(<CitySearch />);
+  test("change state when text input changes", () => {
+    // const CitySearchWrapper = shallow(<CitySearch />);
     const eventObject = { target: { value: "Berlin" } }; 
     CitySearchWrapper.find(".city").simulate("change", eventObject);
-    expect(CitySearchWrapper.state("query")).toBe("Berlin");
+      expect(CitySearchWrapper.state("query")).toBe("Berlin");
+    });
+  test("render list of suggestions correctly", () => {
+    // const CitySearchWrapper = shallow(<CitySearch />);
+    const suggestions = CitySearchWrapper.state("suggestions"); // compares suggestions with state value
+      expect(CitySearchWrapper.find(".suggestions li")).toHaveLength(
+      suggestions.length,
+    );
+    for (let i = 0; i < suggestions.length; i += 1) { // loops through all the suggestions and compares the items in suggestions one by one.
+      expect(
+        CitySearchWrapper.find(".suggestions li")
+          .at(i)
+          .text(),
+      ).toBe(suggestions[i].name_string);
+    }
+    });
+    test("click on suggestion should change query state", () => {
+      CitySearchWrapper.setState({// control CitySearch component state while testing
+        suggestions: [
+          {
+            city: "Munich",
+            country: "de",
+            localized_country_name: "Germany",
+            name_string: "Munich, Germany",
+            zip: "meetup3",
+            lat: 48.14,
+            lon: 11.58,
+          },
+          {
+            city: "Munich",
+            country: "us",
+            localized_country_name: "USA",
+            state: "ND",
+            name_string: "Munich, North Dakota, USA",
+            zip: "58352",
+            lat: 48.66,
+            lon: -98.85,
+          },
+        ],
+      });
+      CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
+      expect(CitySearchWrapper.state('query')).toBe('Munich, Germany');
     });
 });
