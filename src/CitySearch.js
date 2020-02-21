132523f8c04fabb3588cc4de4a099bc08b36bfbf
cgobbet <cassiano.gobbet@gmail.com>
2020-02-21 14:38:06 +0100

    City location, search and suggestions tests done

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,24 +1,41 @@
 import React, { Component } from "react";
 
 class CitySearch extends Component {
-  state = { // define query state for CitySearch component
-    query: 'Munich',
-  }
+  state = {
+    // define query state for CitySearch component
+    query: "", // defines query following API documentation
+    suggestions: [], // defines suggestions in CitySearch state following API documentation
+  };
 
-  handleInputChanged = (event) => {
-  const value = event.target.value;
-  this.setState({ query: value });
-  }
+  handleInputChanged = event => {
+    const value = event.target.value;
+    this.setState({ query: value });
+  };
+  handleItemClicked = value => {
+    //handler that selects clicked suggestion
+    this.setState({ query: value });
+  };
   render() {
     return (
       <div className='CitySearch'>
-        <input // adds city element
+        <input // adds city element textbox
           type='text'
           className='city'
           value={this.state.query} //<input> will derive its value from the value of query (which is located in the state of CitySearch).
           onChange={this.handleInputChanged}
         />
-        <ul className='suggestions'></ul>
+        <ul className='suggestions'>
+          {this.state.suggestions.map((
+            item, //render suggestion list items using name_string to diff
+          ) => (
+            <li
+              key={item.name_string}
+              onClick={() => this.handleItemClicked(item.name_string)}
+            >
+              {item.name_string}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
