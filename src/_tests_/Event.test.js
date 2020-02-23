6c3e439cd0e16144dc2ff2bd6461395d830d2dc3
cgobbet <cassiano.gobbet@gmail.com>
2020-02-23 14:19:32 +0100

    Removal of repeated variables EventWrapper

--- src/_tests_/Event.test.js
+++ src/_tests_/Event.test.js
@@ -9,27 +9,22 @@ describe("<Event /> component", () => {
     EventWrapper = shallow(<Event />);
   });
 test("render event details", () => {
-    // const EventWrapper = shallow(<Event />);
     expect(EventWrapper.find(EventDetails)).toHaveLength(1);
   });
 test("render details collapsed", () => {
-    // const EventWrapper = shallow(<Event />);
     expect(EventWrapper.find(EventDetails).prop('isOpen')).toEqual(false);
   });
 test("render details visible when click", () => {
-    // const EventWrapper = shallow(<Event />);
     expect(EventWrapper.state('showDetails')).toEqual(false);
     EventWrapper.find('.details').simulate('click');
     expect(EventWrapper.find(EventDetails).prop('isOpen')).toEqual(true);
   });
 test("render details collapsed when click", () => {
-    // const EventWrapper = shallow(<Event />);
     expect(EventWrapper.state('showDetails')).toEqual(true);
     EventWrapper.find('.details').simulate('click');
     expect(EventWrapper.find(EventDetails).prop('isOpen')).toEqual(false);
   });
 // test("render details visible", () => {
-//     // const EventWrapper = shallow(<Event />);
 //     expect(EventWrapper.find(EventDetails).prop('isOpen')).toEqual(true);
 //   });
 }); 
