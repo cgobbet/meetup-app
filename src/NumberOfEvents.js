f11477af7252cf76aef26f1a42b1c43aa65fe5ad
cgobbet <cassiano.gobbet@gmail.com>
2020-02-23 15:13:07 +0100

    Adds function to increase amount of events shown

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -4,12 +4,19 @@ class NumberOfEvents extends Component {
   state = {
     amount: 32,
   }
+
+  onClick = () => {
+    this.setState({
+      amount: this.state.amount + 1
+    })
+  }
   
   render() {
     return (
       <div className='NumberOfEvents'>
         <span>Showing</span>
         <input value={this.state.amount} />
+        <button onClick={this.onClick}>+1</button>
       </div>
     );
   }
