a72ce15622687306b4b8b666d7755ad04e492480
cgobbet <cassiano.gobbet@gmail.com>
2020-02-24 21:46:25 +0100

    Update EventDetails.js
    Adding brackets to the return statement as it does have multiple lines.

--- src/EventDetails.js
+++ src/EventDetails.js
@@ -3,9 +3,11 @@ import React, { Component } from "react";
 class EventDetails extends Component {
   render() {
     return 
-      <div className='EventDetails'>
+      (
+        <div className='EventDetails'>
         Event.Name Event.Date Event.id
-      </div>;
+      </div>
+      );
   }
 }
 
