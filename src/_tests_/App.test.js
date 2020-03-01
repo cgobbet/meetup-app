48c8c64333bf3a0da3e58e9ea03498cd40130dc9
cgobbet <cassiano.gobbet@gmail.com>
2020-03-01 12:35:42 +0100

    Inclusion of integration tests
    Mount app, update function and app with children access and mocks data tocall function

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -3,7 +3,10 @@ import CitySearch from "../CitySearch";
 import EventList from "../EventList";
 import NumberOfEvents from "../NumberOfEvents";
 import React from "react";
+import { mockEvents } from "../mock-events";
+import { mount } from "enzyme";
 import { shallow } from "enzyme";
+import { unmount } from "enzyme";
 
 describe("<App /> component", () => {
   let AppWrapper;
@@ -22,3 +25,19 @@ describe("<App /> component", () => {
     expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
   });
 });
+
+describe("<App /> integration", () => {
+  test("get list of events after user selects a city", async () => {
+    const AppWrapper = mount(<App />); // mount because we need to render the component’s children
+    AppWrapper.instance().updateEvents = jest.fn(); //tell Jest to execute update function on the component so results can be seen
+    AppWrapper.instance().forceUpdate(); //update the App component
+    const CitySearchWrapper = AppWrapper.find(CitySearch); //full rendering gives access to children component and its functions like handleItemClicked()
+    CitySearchWrapper.instance().handleItemClicked("value", 1.1, 1.2); // value on parameter not relevant; used because the function needs to be called with a value
+    expect(AppWrapper.instance().updateEvents).toHaveBeenCalledTimes(1);
+    expect(AppWrapper.instance().updateEvents).toHaveBeenCalledWith(1.1, 1.2); 
+    //checks if updateEvents() has been called with the same lat/lon parameters
+  });
+});  
+/*  
+At the end of your previous test, add a new line, AppWrapper.unmount();, then create a new test, this one to change the state after getting the list of events:
+*/
