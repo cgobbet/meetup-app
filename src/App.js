8902e6cdb901691bc431411d0776eea2308aa147
cgobbet <cassiano.gobbet@gmail.com>
2020-03-01 16:48:30 +0100

    Update App.js
    Declare initial state

--- src/App.js
+++ src/App.js
@@ -7,10 +7,18 @@ import EventList from './EventList';
 import NumberOfEvents from "./NumberOfEvents";
 
 class App extends Component {
+  state = {
+    events: [],
+    page: null,
+    lat: null,
+    lon: null,
+  };
+  
   render() {
     return (
-      <div className="App">
-        <CitySearch />
+      <div className='App'>
+        <CitySearch updateEvents={this.updateEvents} />
+        {/* pass it as a prop to your CitySearch component */}
         <EventList />
         <NumberOfEvents />
       </div>
