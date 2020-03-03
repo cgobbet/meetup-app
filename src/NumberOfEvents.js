60343132c2659333509a9913cf0ee3ee6ddabf5d
cgobbet <cassiano.gobbet@gmail.com>
2020-03-03 13:06:47 +0100

    Re-write of NumberOfEvents
    Re-write to make suggestions work while user is typing the city

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,27 +1,25 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
   state = {
-    amount: 32,
-  }
+    amount: 12 //defines default amount of events displayed
+  };
 
-  onClick = () => {
-    this.setState({
-      amount: this.state.amount + 1
-    })
-  }
-  
+  handleInputChanged = event => {
+    const value = event.target.value;
+    this.setState({ amount: value });//changes state and defines amount of events displayed
+  };
+
   render() {
     return (
-      <div className='NumberOfEvents'>
-        <span>Showing</span>
+      <div className="NumberOfEvents">Showing the next
         <input
-          type='text'
-          className='city'
+          type="text"
+          className="eventsDisplayed"
+          placeholder="Set number of events shown"
           value={this.state.amount}
-          onChange={event => this.onClick(event)}
-        />
-        <button onClick={this.onClick}>+1</button>
+          onChange={this.handleInputChanged}
+        /> events
       </div>
     );
   }
@@ -29,4 +27,7 @@ class NumberOfEvents extends Component {
 
 export default NumberOfEvents;
 
-// line 22: "onChange" handler added to input to make company to "Value"
+//15: determines text box name in the tests
+//18: determines the actual number of events to be events displayed
+//20: exhibits the default number of events events displayed
+//21: activates handleInputChanged function on change
