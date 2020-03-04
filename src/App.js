e38c574ff3345b63b874c9ba120baa268cf77404
cgobbet <cassiano.gobbet@gmail.com>
2020-03-04 23:01:32 +0100

    4.4 1st submission

--- src/App.js
+++ src/App.js
@@ -9,11 +9,30 @@ import { getEvents } from './api';
 
 class App extends Component {
   state = {
-    events: []
+    events: [],
+    page: null,
+    lat: null,
+    lon: null
   };
 
-  updateEvents = (lat, lon) => {
-    getEvents(lat, lon).then(events => this.setState({ events }));
+  // componentDidMount() {
+  //   this.updateEvents();
+  // }
+
+  updateEvents = (lat, lon, page) => {
+    if (lat && lon) {
+      getEvents(lat, lon, this.state.page).then(events =>
+        this.setState({ events, lat, lon }),
+      );
+    } else if (page) {
+      getEvents(this.state.lat, this.state.lon, page).then(events =>
+        this.setState({ events, page }),
+      );
+    } else {
+      getEvents(this.state.lat, this.state.lon, this.state.page).then(events =>
+        this.setState({ events }),
+      );
+    }
   };
 
   render() {
