9589b9ac68a21cfe79031f2f05783311a98c22b3
cgobbet <cassiano.gobbet@gmail.com>
2020-03-04 12:51:51 +0100

    Update CitySearch.js
    Include message for user when no city has been found

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -6,25 +6,40 @@ class CitySearch extends Component {
   state = {
     // define query state for CitySearch component
     query: "", // defines query following API documentation
-    suggestions: [] // defines suggestions in CitySearch state following API documentation
+    suggestions: [], // defines suggestions in CitySearch state following API documentation
   };
 
-  handleInputChanged = (event) => { // whether any textual changes have been made
+  handleInputChanged = event => {
     const value = event.target.value;
     this.setState({ query: value });
-    getSuggestions(value).then(suggestions => this.setState({ suggestions }))
-  };//retrieves suggestions according to value input
+    getSuggestions(value).then(suggestions => {
+      this.setState({ suggestions });
+
+      if (value && suggestions.length === 0) {
+        this.setState({
+          alert: "We cannot find that city",
+        });
+      } else {
+        this.setState({
+          alert: "",
+        });
+      }
+    });
+  };
 
   handleItemClicked = (value, lat, lon) => {
-    this.setState({ query: value, suggestions: [] });// set initial state and suggestions array start empty
+    this.setState({ query: value, suggestions: [] }); // set initial state and suggestions array start empty
     this.props.updateEvents(lat, lon);
   };
   render() {
     return (
-      <div className='CitySearch'> Show me events around: 
+      <div className='CitySearch'>
+        {" "}
+        Show me events around
         <input // adds city element text box
           type='text'
           className='city'
+          placeholder='Which city?'
           value={this.state.query} //<input> will derive its value from the value of query (which is located in the state of CitySearch).
           onChange={this.handleInputChanged}
         />
@@ -32,7 +47,14 @@ class CitySearch extends Component {
           {this.state.suggestions.map((
             item, //render suggestion list items using name_string to diff
           ) => (
-            <li key={item.name_string} onClick={() => this.handleItemClicked(item.name_string, item.lat, item.lon)}>{item.name_string}</li>
+            <li
+              key={item.name_string}
+              onClick={() =>
+                this.handleItemClicked(item.name_string, item.lat, item.lon)
+              }
+            >
+              {item.name_string}
+            </li>
           ))}
         </ul>
       </div>
