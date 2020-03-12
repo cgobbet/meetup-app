3f141f61d7695b2ad594c6e1b5c7a13b45e1626e
cgobbet <cassiano.gobbet@gmail.com>
2020-03-12 14:54:08 +0100

    4.9 Submission - Event Charts

--- src/Event.js
+++ src/Event.js
@@ -1,9 +1,18 @@
+import {
+  Cell,
+  Legend,
+  Line,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
 import React, { Component } from "react";
 
 class Event extends Component {
   state = {
+    showDetails: false,
     event: [],
-    showDetails: false
   };
 
   handleDetails = () => {
@@ -14,9 +23,24 @@ class Event extends Component {
     }
   };
 
+  getAttendance = () => {
+    const reserved = this.props.event.yes_rsvp_count;
+    const max = this.props.event.rsvp_limit;
+    const places = max - this.props.event.yes_rsvp_count
+
+    return (
+      [
+        {"name": "People Going", value: reserved},
+        {"name": "Still Open", "value": places}
+      ]
+    )
+  }
+
   render() {
     // const showDetails = this.state.showDetails;
     const { event } = this.props;
+
+    let colors = ["#B22222", "#FFD700"];
 
     return (
       <div className='event'>
@@ -24,11 +48,53 @@ class Event extends Component {
           <p className='eventWrap--name'>{this.props.event.name}</p>
           <p className='eventWrap--localDate'>{this.props.event.local_date}</p>
           <p className='eventWrap--localTime'>{this.props.event.local_time}</p>
-          <button className="button" onClick={() => this.handleDetails()}>+ Details</button>
+          <button className='button' onClick={() => this.handleDetails()}>
+            + Details
+          </button>
         </div>
         {this.state.showDetails && (
           <div className='eventDetails'>
-            <p className='eventDetails--description'dangerouslySetInnerHTML={{ __html: event.description }}/>
+            {event.yes_rsvp_count && event.rsvp_limit ? (
+              <ResponsiveContainer height={180}>
+                <PieChart width={180} height={180}>
+                  <Pie
+                    data={this.getAttendance()}
+                    dataKey='value'
+                    nameKey='name'
+                    cx='50%'
+                    cy='50%'
+                    innerRadius={40}
+                    outerRadius={70}
+                    label
+                  >
+                    {this.getAttendance().map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={colors[index]} />
+                    ))}
+                  </Pie>
+
+                  <Tooltip />
+
+                  <Legend verticalAlign='top' height={36}>
+                    <Line
+                      name='Attending'
+                      type='stepBefore'
+                      dataKey='reserved'
+                      stroke='#483D8B'
+                    />
+                    <Line
+                      name='Spots Open'
+                      type='stepBefore'
+                      dataKey='places'
+                      stroke='#006400'
+                    />
+                  </Legend>
+                </PieChart>
+              </ResponsiveContainer>
+            ) : null}
+            <p
+              className='eventDetails--description'
+              dangerouslySetInnerHTML={{ __html: event.description }}
+            />
           </div>
         )}
       </div>
